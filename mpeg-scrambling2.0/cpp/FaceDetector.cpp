c5d809981db0937a9d8765cfafe3b5ab1f5dbfe2
Antonio Robustelli <tony.robustelli95@hotmail.com>
2019-01-28 23:48:20 +0100

    Update FaceDetector.cpp

--- mpeg-scrambling2.0/cpp/FaceDetector.cpp
+++ mpeg-scrambling2.0/cpp/FaceDetector.cpp
@@ -3,6 +3,7 @@
 #include"FaceDetector.h"
 #include"opencv2/core/types.hpp"
 #include<exception>
+#include<limits>
 
 using namespace std;
 using namespace cv;
@@ -49,7 +50,7 @@ vector<Rect> FaceDetector::checkOverlapResult(vector<Rect> rects)
                     if (overlaps(rects.at(i), rects.at(j)))
                     {
                         cout<<("overlap");
-                        Rect r = rects.at(i).union(rects.at(j));
+                        Rect r = myunion(rects.at(i),rects.at(j));
                         rects.erase(rects.begin() + i);
                         rects.insert(rects.begin() + i, r);
                         rects.erase(rects.begin() + j);
@@ -76,4 +77,48 @@ bool FaceDetector::mycontains(Rect r1, Rect r2)
     return r1.width > 0 && r1.height > 0 && r2.width > 0 && r2.height > 0
     && r2.x >= r1.x && r2.x + r2.width <= r1.x + r1.width
     && r2.y >= r1.y && r2.y + r2.height <= r1.y + r1.height;
+}
+
+Rect FaceDetector::myunion(Rect r1, Rect r2)
+{
+    long tx2 = r1.width;
+    long ty2 = r1.height;
+    if ((tx2 | ty2) < 0) 
+    {
+        // This rectangle has negative dimensions...
+        // If r has non-negative dimensions then it is the answer.
+        // If r is non-existant (has a negative dimension), then both
+        // are non-existant and we can return any non-existant rectangle
+        // as an answer.  Thus, returning r meets that criterion.
+        // Either way, r is our answer.
+        return r2;
+    }
+
+    long rx2 = r2.width;
+    long ry2 = r2.height;
+    if ((rx2 | ry2) < 0) 
+    {
+        return r1;
+    }
+
+    int tx1 = r1.x;
+    int ty1 = r1.y;
+    tx2 += tx1;
+    ty2 += ty1;
+    int rx1 = r2.x;
+    int ry1 = r2.y;
+    rx2 += rx1;
+    ry2 += ry1;
+    if (tx1 > rx1) tx1 = rx1;
+    if (ty1 > ry1) ty1 = ry1;
+    if (tx2 < rx2) tx2 = rx2;
+    if (ty2 < ry2) ty2 = ry2;
+    tx2 -= tx1;
+    ty2 -= ty1;
+    // tx2,ty2 will never underflow since both original rectangles
+    // were already proven to be non-empty
+    // they might overflow, though...
+    if (tx2 > numeric_limits<int>::max()) tx2 = numeric_limits<int>::max();
+    if (ty2 > numeric_limits<int>::max()) ty2 = numeric_limits<int>::max();
+    return Rect(tx1, ty1, (int) tx2, (int) ty2);
 }
