f3f6d75051aef094e2c1773a8aa38f0c3ffe568f
Antonio Robustelli <tony.robustelli95@hotmail.com>
2019-01-28 12:23:32 +0100

    Create FaceDetector.h for FaceDetector.cpp

---
+++ mpeg-scrambling2.0/cpp/FaceDetector.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include"FaceDetectorInterface.h"
+#include"opencv2/core/types.hpp"
+
+using namespace std;
+using namespace cv;
+
+class FaceDetector
+{
+    private: 
+            vector<FaceDetectorInterface<Rect>> strategies;
+            vector<Rect> checkOverlapResult(vector<Rect> rects);
+            static bool overlaps (Rect r1, Rect r2);
+
+    public: 
+            FaceDetector();
+            void addStrategy(FaceDetectorInterface<Rect> strategy);
+};
