1291ece67d3a89523aee297568b7e6299381ee75
Antonio Robustelli <tony.robustelli95@hotmail.com>
2019-01-29 10:35:37 +0100

    Partial porting Scrambling.java to Scrambling.cpp

---
+++ mpeg-scrambling2.0/cpp/Scrambling.cpp
@@ -0,0 +1,202 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<exception>
+#include"opencv2/core/types.hpp"
+#include"opencv2/imgcodecs.hpp"
+#include"opencv2/objdetect/objdetect.hpp"
+#include"opencv2/core/mat.hpp"
+#include<stdlib.h>
+
+using namespace std;
+using namespace cv;
+
+bool overlaps(Rect r1, Rect r2);
+bool mycontains(Rect r1, Rect r2);
+Rect myunion(Rect r1, Rect r2);
+vector<Rect> checkOverlapResult(vector<Rect> rects);
+
+int main(int argc, char *argv[])
+{		
+    int w = 0;
+    int h = 0;
+    string path = "";
+    string file_format = "bmp";
+    int rgb;
+    
+    w = atoi(argv[0]);
+    h = atoi(argv[1]);
+    path = argv[2]; 
+    
+    string n_frame = argv[3];
+
+    string fileY = path+"~Y.txt";
+    string fileU = path+"~U.txt";
+    string fileV = path+"~V.txt";
+        
+    cout<<"Scrambling...  frame: "<<n_frame<<endl;
+    cout<<"Apro il file "<<fileY<<endl;
+    cout<<"Apro il file "<<fileU<<endl;
+    cout<<"Apro il file "<<fileV<<endl;
+
+    //more code
+    
+    //////////////////////////////////////////////////////////////////////////
+    // APPLICO LO SCRAMBLING SULLA LUMINANZA DEL FRAME PER CALCOLARE LE ROI //
+    //////////////////////////////////////////////////////////////////////////
+        
+    vector<string> strategies;
+    strategies.push_back("../xml/lbpcascade_frontalface.xml");
+    strategies.push_back("../xml/lbpcascade_frontalcatface.xml");
+    strategies.push_back("../xml/lbpcascade_frontalface_improved.xml");
+    strategies.push_back("../xml/lbpcascade_profileface.xml");
+    strategies.push_back("../xml/lbpcascade_silverware.xml");
+    strategies.push_back("../xml/haarcascade_eye.xml");
+    strategies.push_back("../xml/haarcascade_profileface.xml");
+    strategies.push_back("../xml/haarcascade_frontalface_alt.xml");
+    
+    
+    // DEFINISCO LA STRATEGIA DI RICERCA
+    //Mat image; //= imread(""/*outputfileY.getAbsolutePath()*/,1);
+    
+    /*if(image.empty()) 
+    {
+        cout<<"Immagine non caricata!"<<endl;
+        return -1;
+    }*/
+    
+    vector<Point> pts;
+    //CascadeClassifier faceDetector;
+    vector<Rect> rettangoli;
+    
+    int faces = 0;
+    /*for(string strategy : strategies) 
+    {
+        
+        faceDetector = CascadeClassifier(strategy);
+        
+        vector<Rect> faceVectors;
+        //faceDetector.detectMultiScale(image, faceVectors);
+
+        if(faceVectors.size() > faces) 
+        {
+            faces = faceVectors.size();
+        }
+    
+        for(Rect rect : faceVectors) 
+        {
+            rettangoli.push_back(Rect(rect.x, rect.y, rect.width, rect.height));
+        }
+        
+        rettangoli = checkOverlapResult(rettangoli);
+    }*/
+
+    //more code
+
+    return 0;
+}
+
+bool overlaps(Rect r1, Rect r2) 
+{
+    return r2.x < r1.x + r1.width && r2.x + r2.width > r1.x && r2.y < r1.y + r1.height && r2.y + r2.height > r1.y;
+}
+
+bool mycontains(Rect r1, Rect r2)
+{
+    return r1.width > 0 && r1.height > 0 && r2.width > 0 && r2.height > 0
+    && r2.x >= r1.x && r2.x + r2.width <= r1.x + r1.width
+    && r2.y >= r1.y && r2.y + r2.height <= r1.y + r1.height;
+}
+
+Rect myunion(Rect r1, Rect r2)
+{
+    long tx2 = r1.width;
+    long ty2 = r1.height;
+    if ((tx2 | ty2) < 0) 
+    {
+        // This rectangle has negative dimensions...
+        // If r has non-negative dimensions then it is the answer.
+        // If r is non-existant (has a negative dimension), then both
+        // are non-existant and we can return any non-existant rectangle
+        // as an answer.  Thus, returning r meets that criterion.
+        // Either way, r is our answer.
+        return r2;
+    }
+
+    long rx2 = r2.width;
+    long ry2 = r2.height;
+    if ((rx2 | ry2) < 0) 
+    {
+        return r1;
+    }
+
+    int tx1 = r1.x;
+    int ty1 = r1.y;
+    tx2 += tx1;
+    ty2 += ty1;
+    int rx1 = r2.x;
+    int ry1 = r2.y;
+    rx2 += rx1;
+    ry2 += ry1;
+    if (tx1 > rx1) tx1 = rx1;
+    if (ty1 > ry1) ty1 = ry1;
+    if (tx2 < rx2) tx2 = rx2;
+    if (ty2 < ry2) ty2 = ry2;
+    tx2 -= tx1;
+    ty2 -= ty1;
+    // tx2,ty2 will never underflow since both original rectangles
+    // were already proven to be non-empty
+    // they might overflow, though...
+    if (tx2 > numeric_limits<int>::max()) tx2 = numeric_limits<int>::max();
+    if (ty2 > numeric_limits<int>::max()) ty2 = numeric_limits<int>::max();
+    return Rect(tx1, ty1, (int) tx2, (int) ty2);
+}
+
+vector<Rect> checkOverlapResult(vector<Rect> rects) 
+{
+    bool overlap = true;
+    while(overlap) 
+    {
+        overlap=false;
+        try 
+        {
+            for (int i = 0; i < rects.size(); i++) 
+            {
+                for (int j = 0; j < rects.size(); j++) 
+                {
+                    if (i == j) continue;
+                    if (mycontains(rects.at(i),rects.at(j))) 
+                    {
+                        rects.erase(rects.begin() + j);
+                        overlap = true;
+                        j--;
+                        if (i == rects.size()) i--;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < rects.size(); i++)
+            {
+                for (int j = 0; j < rects.size(); j++) 
+                {
+                    if (i == j) continue;
+                    if (overlaps(rects.at(i), rects.at(j)))
+                    {
+                        cout<<("overlap");
+                        Rect r = myunion(rects.at(i),rects.at(j));
+                        rects.erase(rects.begin() + i);
+                        rects.insert(rects.begin() + i, r);
+                        rects.erase(rects.begin() + j);
+                        overlap = true;
+                        j--;
+
+                    }
+
+                }
+            }
+        } 
+        catch (exception e) {}
+    }
+    return rects;
+}
